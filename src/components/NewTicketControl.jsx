2aef4fdcf0325082ad4d25ce1019a33b311f5f01
Joe Pritchett <joempritchett@gmail.com>
2019-04-23 09:09:16 -0700

    add conditional rendering

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -7,12 +7,18 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState({formVisibleOnPage: true});
   }
 
   render(){
     return(
       <div>
         <p>This is the New Ticket Control component</p>
+        <strong onClick={this.handleClick}>Click me to change my state</strong>
       </div>
     );
   }
